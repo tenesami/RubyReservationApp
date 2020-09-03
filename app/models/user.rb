a2d4d98affe068e683b37241520d9d1215e24c67
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-09-03 14:44:45 -0600

    add has secure password validation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
     has_many :reviews
+    has_many :reservations #user crated 
     has_many :reviewed_reservations, through: :reviews, source: :reservation
 
-    has_many :reservations
+    #since active record take of the validation 
+    #we don't need to validate twice 
+    has_secure_password
+
+    #validates :user_name, uniqueness: true, presence: true
+        
+
 end
