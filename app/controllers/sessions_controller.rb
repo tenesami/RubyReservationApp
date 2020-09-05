63000a5e4c66d260d9c018297d75eec49434452f
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-09-04 21:09:11 -0600

    add login in the session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,35 @@
 class SessionsController < ApplicationController
+    
     def home
+        @user = User.new
     end
 
+    #logout the user
     def destroy
         #will not clear everything in the session if you want 
-        #to remove everty thing in the session use session.clear
-        session.delete(:user_id)
+        #to remove everything in the session use session.clear
+        #session.delete(:user_id)
+        session.clear
         redirect_to '/'
     end
 
     def new
     end
 
+    #log in user to the app 
     def create
+        #binding.pry
         #check dose the user exit the in our system
         #find_by through you error  
-        @user = User.find_by_id(user_name: params[:user][:user_name])
+        @user = User.find_by(user_name: params[:user][:user_name])
 
-        #did we find someone & did they put the right password
-        if @user && user.authenticate(params[:user][:password])
+        #find someone &  check they put the right password
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user) 
+        else
+            flash[:notice] = "Incorect login info please try again"
+            redirect_to login_path
         end
     end
 end
