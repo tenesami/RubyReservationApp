b1e2cd48c60c554ecd9889905ff7ea9e87ec19c6
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-09-07 19:31:38 -0600

    add new and create method in the reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,24 @@
 class ReservationsController < ApplicationController
+    def new
+        @reservation = Reservation.new
+        @reservation.build_restaurant #build new restaurant
+    end
+ 
+    def create
+        @reservation = Reservation.new(reservation_params)
+        @reservation.user_id = session[:user_id] #since reservation belong to user 
+
+        if @reservation.save #validaties the reservation
+            redirect_to reservation_path(@reservation)
+        else
+            #@reservation.build_restaurant
+            render :new
+        end
+    end
+
+    def reservation_params
+        params.require(:reservation).permit(:number_of_tables, 
+        :checkin_date, :checkin_time, :checkout_time, 
+        :description, :restaurant_id, restaurant_attributes: [:restaurant_name])
+    end
 end
