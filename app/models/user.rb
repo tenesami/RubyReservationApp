78b1cd2b9547d8cd758a8176143fac28ef6a5e41
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-09-09 19:33:23 -0600

    fix add add logic to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-    has_many :reviews
+    has_many :restaurants
     has_many :reservations #user crated 
-    has_many :reviewed_reservations, through: :reviews, source: :reservation
+    has_many :reserved_restaurants, through: :reservations, source: :restaurant 
 
     #since active record take of the validation 
     #we don't need to validate twice 
     has_secure_password
 
-    validates :user_name, uniqueness: true, presence: true
+    validates :username, uniqueness: true, presence: true
         
 
 end
