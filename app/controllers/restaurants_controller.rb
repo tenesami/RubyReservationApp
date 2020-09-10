b1dce713aa627dfbce19691fc9b6c21daa3b1cce
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-09-10 10:44:27 -0600

    add and edit restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,41 @@
 class RestaurantsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+    
+    def new
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+            @restaurant = @user.restaurants.build
+          else
+            @restaurant = Restaurant.new
+          end
+    end
+
+    def index
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+            @restaurants = @user.restaurants
+        else
+            @error = "These user doesn't exitrs" if params[:user_id]
+            @restaurants = Restaurant.all
+        end
+    end
+
+    def create
+        @restaurant = current_user.restaurants.build(rest_params)
+        if @restaurant.save
+            redirect_to restaurants_path
+        else
+            render :new
+        end
+    end
+
+    
+    def show
+        @restaurant = Restaurant.find_by_id(params[:id])
+        redirect_to restaurants_path if !@restaurant
+    end
+
+    private
+    def rest_params
+        params.require(:restaurant).permit(:restaurant_name, :available_table)
+    end
+
 end
