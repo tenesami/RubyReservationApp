0cda74809d5a3e5b682e0f5fbef8f88f1b216b58
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-09-10 15:05:21 -0600

    add scope and alpha method in restarant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,7 @@ class Restaurant < ApplicationRecord
     belongs_to :user
     has_many :reservations
     has_many :users, through: :reservations
+
+    scope :alpha, -> {order(:restaurant_name)} 
+    scope :most_reservations, -> { Restaurant.joins(:reservations).group('restaurants.id').order('count(restaurants.id) desc')}
 end
