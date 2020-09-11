3bc3318930e3cde98cb1722690bbc82e9ba75768
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-09-11 14:19:14 -0600

    add custom validation in the restaurant  model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,21 @@ class Restaurant < ApplicationRecord
     has_many :reservations
     has_many :users, through: :reservations
 
+    validates :restaurant_name,  presence: true
+
+    validate :too_many_restaurants
+
     scope :alpha, -> {order(:restaurant_name)} 
     scope :most_reservations, -> { Restaurant.joins(:reservations).group('restaurants.id').order('count(restaurants.id) desc')}
+
+    def too_many_restaurants
+        #binding.pry
+        today_restaurants = user.restaurants.select do |r|
+            r.created_at.try(:to_date) == Date.today
+        end
+        if today_restaurants.size > 2
+            errors.add(:restaurants_id, "can not be added more than 2 times per day")
+        end
+    end
+
 end
