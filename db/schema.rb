2cfba7b0b2a6aeefd43d6b7d3cb292122bf320f6
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-09-12 00:18:57 -0600

    update schema to fix error

--- db/schema.rb
+++ db/schema.rb
@@ -32,18 +32,6 @@ ActiveRecord::Schema.define(version: 2020_09_03_052219) do
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.integer "stars"
-    t.string "title"
-    t.string "content"
-    t.integer "user_id"
-    t.integer "reservation_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["reservation_id"], name: "index_reviews_on_reservation_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
