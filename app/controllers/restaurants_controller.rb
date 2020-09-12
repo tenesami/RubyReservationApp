a450aba7c9496a929253ac8ea7f16dcca45a3145
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-09-12 00:27:37 -0600

    updare restaurant controller with custom validation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,30 +11,44 @@ class RestaurantsController < ApplicationController
 
     def index
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
-            @restaurants = @user.restaurants.most_reservations
+            @restaurants = @user.restaurants.alpha
         else
             @error = "These user doesn't exitrs" if params[:user_id]
-            @restaurants = Restaurant.most_reservations
+            @restaurants = Restaurant.alpha.includes(:user)
         end
     end
 
     def create
-        @restaurant = current_user.restaurants.build(rest_params)
+        @restaurant = current_user.restaurants.build(restaurant_params)
         if @restaurant.save
-            redirect_to restaurants_path
+            redirect_to restaurants_path(@restaurant)
         else
             render :new
         end
     end
 
+    def edit
+        @restaurant = Restaurant.find_by_id(params[:id])
+        redirect_to restaurants_path if !@restaurant || @restaurant.user != current_user
+      end
     
+    def update
+    @restaurant = Restaurant.find_by(id: params[:id])
+    redirect_to restaurants_path if !@restaurant || @restaurant.user != current_user
+        if @restaurant.update(restaurant_params)
+        redirect_to restaurant_path(@restaurant)
+        else
+        render :edit
+        end
+    end
+
     def show
         @restaurant = Restaurant.find_by_id(params[:id])
         redirect_to restaurants_path if !@restaurant
     end
 
     private
-    def rest_params
+    def restaurant_params
         params.require(:restaurant).permit(:restaurant_name, :available_table)
     end
 
