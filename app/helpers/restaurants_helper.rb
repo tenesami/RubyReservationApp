51f85f8b755075f17d14db3042016793b7cf6f84
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-09-12 00:33:42 -0600

    create resturant helper method in helper folder

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,20 @@
 module RestaurantsHelper
+
+    def index_display_header
+        if @user
+          content_tag(:h1, "#{@user.username}'s Restaurants:")
+        else
+          content_tag(:h2, "All Restaurants")
+        end
+      end
+    
+      def display_restaurants
+        if @user.restaurants.empty?
+          tag.h2(link_to('No restaurants yet - write a restaurants here', new_restaurant_path))
+        else
+          user = @user == current_user ? 'Your' : "#{@user.username}'s"
+          content_tag(:h2, "#{user} #{pluralize(@user.restaurants.count, 'Restaurant')}:")
+    
+        end
+      end
 end
